1225827c7ff6e0d6c2fe79b8b8b9142c20f2fc6a
Ali Akbar <aliakbr@outlook.co.id>
2016-04-06 17:16:29 +0700

    Baru

--- Manusia.java
+++ Manusia.java
@@ -1,38 +1,60 @@
+package tubes2;
+
 public class Manusia extends Hewan {
+	/**
+     * A private attribute.
+     * It stores the dodge condition.
+     */
+    private boolean menghindar;
 	
 	/**
      * A Constructor.
      * It use default parameters.
-     * @see MakhlukHidup
-	 * @see HuntingSkill
-	 * @see Gerak
      */
-    Manusia();
-
+    Manusia(){
+		super();
+		menghindar = false;
+	}
  
 	/**
 	 * Setter procedure for menghindar attribute
 	 * @param m A first parameter will be assigned in menghindar atrribute
 	 */
-    public void setMenghindar(bool m);
+    public void setMenghindar(boolean m){
+		menghindar  = m;
+	}
 
     /**
      * Getter function for menhindar attribute
      * @return menghindar attribute
      */
-    public bool getMenghindar ();
-
-
-    /**
-     * A procedure changes posisi attribute.
-     * It uses direction that assigned by _arah.
-     * @see Gerak#gerak_berarah()
-     */
-    public void gerak_berarah();
-
-    /**
-     * A private attribute.
-     * It stores the dodge condition.
-     */
-    private bool menghindar;
+    public boolean getMenghindar (){
+		return menghindar;
+	}
+	
+	public void Reaction(MakhlukHidup M){
+		if (M.getPosisi()== getPosisi()){
+			if (get_DNA() == M.get_DNA()) {
+				if (M.get_umur() >= get_umur())
+					setMati(true);
+			} else if (isPredator(M.get_DNA())) {
+					setMati(true);
+			}
+		}else if (isRadius(10,M.getPosisi())){
+			if (isTarget(M.get_DNA()) && (M.get_DNA() != '^')){
+				set_Arah_Memburu(getPosisi(),M.getPosisi());
+				setSedangMemburu(true);
+			}
+			else if (!getSedangMemburu())
+				set_Arah_Bebas();
+		} else if(isRadius(2,M.getPosisi())) {
+			if (isPredator(M.get_DNA()))
+				set_Arah_Menjauh(getPosisi(),M.getPosisi());
+			else if (!getSedangMemburu())
+				set_Arah_Bebas();
+		} else {
+			if (!getSedangMemburu())
+				set_Arah_Bebas();
+		}
+	}
 }
