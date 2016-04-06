86a1d9dc9dc4d43dad93dc6a8503ef8fe5959015
Ali Akbar <aliakbr@outlook.co.id>
2016-04-07 00:51:35 +0700

    WorldTest javaswing

---
+++ Makhluk2/test/makhluk2/WorldTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package makhluk2;
+
+import net.slashie.libjcsi.ConsoleSystemInterface;
+import net.slashie.libjcsi.jcurses.JCursesConsoleInterface;
+import net.slashie.libjcsi.wswing.WSwingConsoleInterface;
+
+/**
+ *
+ * @author Ali-pc
+ */
+public class WorldTest {
+    public static void main(String[] args) {
+        World W = new World();
+        W.initDisplay();
+        
+        /*Point P = new Point(15,15);
+        ConsoleSystemInterface csi = new WSwingConsoleInterface("wawa");
+        
+        try {
+             
+                 csi.cls();
+                 csi.print(10, 10, "b");
+                } catch (ExceptionInInitializerError e) {
+
+                } 
+                finally {
+                    
+                }
+        System.out.println("wawaw");*/
+    }
+}
